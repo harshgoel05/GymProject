397956598bc1495942402e9360ba026c5785b106
harshgoel05 <harshgoel05@gmail.com>
2020-06-17 11:38:39 +0530

    login api call

--- js/script.js
+++ js/script.js
@@ -4,15 +4,17 @@ $('#login-btn').on('click', function (event) {
 	var email = $('#email').val();
 	var pass = $('#pass').val();
 	var url = 'http://gymworkout.pe.hu/api/api1.php?user=' + email + '&pass=' + pass;
+	// url = 'https://quotes.rest/qod?language=en';
 	if (email && pass) {
 		$.ajax({
-			method: 'POST',
 			url: url,
-			headers: { 'Content-Type': 'application/json' },
-
-			success: function (result) {
-				alert(result);
-				window.location.replace('https://google.com');
+			type: 'GET',
+			crossDomain: true,
+			success: function (response) {
+				console.log(response);
+			},
+			error: function (xhr, status) {
+				console.log('error', xhr, status);
 			},
 		});
 	} else {
