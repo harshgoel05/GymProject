acaa26e2e0a11f18b6ea73e17da81d8d2a5a3720
harshgoel05 <harshgoel05@gmail.com>
2020-08-03 19:28:20 +0530

    timing fix

--- js/time.js
+++ js/time.js
@@ -49,14 +49,72 @@ $('#submit-btn').click(function () {
 	var type = $('.type-f');
 	var minutes = $('.minutes-f');
 	var hours = $('.hours-f');
+	var total = $('#no-of-slot').val();
+	var sub_url = '';
+	let time_arr = [];
+	var mor = 0,
+		eve = 0,
+		aft = 0;
+	for (let i = 0; i < total; i++) {
+		if (type[i].value == 'Morning') mor++;
+		else if (type[i].value == 'Evening') eve++;
+		else if (type[i].value == 'Afternoon') aft++;
+	}
+	for (let i = 0; i < total; i++) {
+		time_arr[i] = hours[i].value.toString();
+	}
+	for (let i = 0; i < total; i++) {
+		// console.log(minutes[i].value);
+		time_arr[i] = time_arr[i].toString() + minutes[i].value.toString();
+	}
+	var gymc = $('#gym').val();
+	var psm = $('#psm').val();
 
-	for (let i = 0; i < type.length; i++) {
-		console.log(type[i].value);
+	for (let i = 0; i < time_arr.length; i++) {
+		time_arr[i] = '&s' + i + '=' + time_arr[i];
+		sub_url = sub_url + time_arr[i];
 	}
-	for (let i = 0; i < minutes.length; i++) {
-		console.log(minutes[i].value);
-	}
-	for (let i = 0; i < hours.length; i++) {
-		console.log(hours[i].value);
-	}
+	//Change url here
+	$.ajax({
+		url:
+			'https://app.mbvgroup.in/websiteapi/api13.php?' +
+			'gymc=' +
+			gymc +
+			'&psm=' +
+			psm +
+			'&total=' +
+			total +
+			'&mor=' +
+			mor +
+			'&aft=' +
+			aft +
+			'&eve=' +
+			eve +
+			sub_url,
+		type: 'GET',
+		crossDomain: true,
+		headers: {
+			accept: 'text/html; charset=UTF-8',
+			'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+			'Access-Control-Allow-Origin': '*',
+		},
+		options: {
+			mode: 'no-cors',
+		},
+		success: function (response) {
+			console.log(response);
+			console.log(response.status);
+			var r = JSON.parse(response);
+			console.log(r.status);
+			if (r.status == 0) {
+				alert('Some error occured!');
+			} else {
+				alert('Successfully changed password!');
+			}
+		},
+		error: function (xhr, status) {
+			console.log('error', xhr, status);
+			alert('Some unknown error occured');
+		},
+	});
 });
